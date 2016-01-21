f264c61605597c5c69fb4ee4337fc27f376bd923
VieVie31 <orissermaroix@gmail.com>
2016-01-21 16:20:04 +0100

    Doc String added

--- src/edu/caravane/guitare/gitobejct/BinaryFile.java
+++ src/edu/caravane/guitare/gitobejct/BinaryFile.java
@@ -8,16 +8,43 @@ import java.util.zip.DataFormatException;
 import java.util.zip.Inflater; 
 
 //13h55 -> 14h05
+//16h00 -> 16h18
+/**
+ * 
+ * @author VieVie31
+ *
+ */
 public class BinaryFile {
+	/**
+	 * Return the content of the file specified by the path argument as
+	 * a byte array.
+	 * 
+	 * @param  path the path of the binary file to read
+	 * @return the content of the file
+	 * @throws IOException
+	 * @author VieVie31
+	 */
 	public static byte[] read(String path) throws IOException {
 		return Files.readAllBytes(Paths.get(path)); //java7 way
 	}
 	
-	public static byte[] decompress(byte[] data) throws IOException, DataFormatException {
+	/**
+	 * Return the decompressed version of the input array using the zlib 
+	 * algorithm.
+	 * 
+	 * @param  data an input byte array to decompress using zlib
+	 * @return the data array decompressed
+	 * @throws IOException
+	 * @throws DataFormatException
+	 * @author VieVie31
+	 */
+	public static byte[] decompress(byte[] data) 
+			throws IOException, DataFormatException {
 		Inflater inflater = new Inflater();
         inflater.setInput(data);
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);  
+        ByteArrayOutputStream outputStream;
+        outputStream = new ByteArrayOutputStream(data.length);  
         byte[] buffer = new byte[1024];  
 
         while (!inflater.finished())
@@ -27,7 +54,18 @@ public class BinaryFile {
         return outputStream.toByteArray();
 	}
 	
-	public static byte[] decompress(String path) throws IOException, DataFormatException {
+	/**
+	 * Return a decompressed file content, using the zlib algorithm ,
+	 * as byte array.
+	 * 
+	 * @param  path of the binary file to decompress
+	 * @return the content of the file decompressed
+	 * @throws IOException
+	 * @throws DataFormatException
+	 * @author VieVie31
+	 */
+	public static byte[] decompress(String path) 
+			throws IOException, DataFormatException {
 		return decompress(read(path));
 	}
 }
