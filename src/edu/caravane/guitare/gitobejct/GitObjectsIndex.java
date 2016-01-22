4b1b4197815de35a1530e110bbb10c1f31127004
VieVie31 <orissermaroix@gmail.com>
2016-01-22 18:29:08 +0100

    Contiendra les object git pour pouvoir etre retrouves par leur valeur de hash/identifiant/nomdefichier

--- src/edu/caravane/guitare/gitobejct/GitObjectsIndex.java
+++ src/edu/caravane/guitare/gitobejct/GitObjectsIndex.java
@@ -2,6 +2,35 @@ package edu.caravane.guitare.gitobejct;
 
 import java.util.HashMap;
 
-public class GitObjectsIndex extends HashMap<GitObject, String> {
+//18h19 -> 18h26
+public class GitObjectsIndex {
+	protected HashMap<String, GitObject> hashMap;
 	
+	public GitObjectsIndex() {
+		hashMap = new HashMap<String, GitObject>();
+	}
+	
+	/**
+	 * This function retrieve a git object (already indexed) by his hash (sha1)
+	 * 
+	 * @author VieVie31
+	 *
+	 * @param  sha1 the hash of the git object to retrieve
+	 * @return a git object
+	 */
+	public GitObject get(String sha1) {
+		return hashMap.get(sha1);
+	}
+	
+	/**
+	 * This function put (index) a git object by his hash value (sha1)
+	 * 
+	 * @author VieVie31
+	 *
+	 * @param sha1 the hash of the git object
+	 * @param gitObject the git object
+	 */
+	public void put(String sha1, GitObject gitObject) {
+		hashMap.put(sha1, gitObject);
+	}
 }
