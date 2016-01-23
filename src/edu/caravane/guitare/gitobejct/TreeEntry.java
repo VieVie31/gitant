5d78bafc00f0cb21cecac09983d3bd9f91395271
VieVie31 <orissermaroix@gmail.com>
2016-01-23 17:41:42 +0100

    creation de l'object TreeEntry qui est ce que contient un object de type GitTree

---
+++ src/edu/caravane/guitare/gitobejct/TreeEntry.java
@@ -0,0 +1,51 @@
+package edu.caravane.guitare.gitobejct;
+
+public class TreeEntry {
+	protected final int octalMode;
+	protected final String name;
+	protected final String sha1;
+	
+	//ne pas faire d'autre constructeur pour que ne soient 
+	//instancies que les objects complets
+	public TreeEntry(int octalMode, String name, String sha1) {
+		this.octalMode = octalMode;
+		this.name = name;
+		this.sha1 = sha1;
+	}
+	
+	//ne pas faire de setter car cet object une fois intancie
+	//doit etre en read only
+	
+	/**
+	 * Getter
+	 * 
+	 * @author VieVie31
+	 *
+	 * @return the octal mode of a file
+	 */
+	public int getOctalMode() {
+		return this.octalMode;
+	}
+	
+	/**
+	 * Getter
+	 * 
+	 * @author VieVie31
+	 *
+	 * @return the name of a file
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	/**
+	 * Getter
+	 * 
+	 * @author VieVie31
+	 *
+	 * @return the sha1 of a file
+	 */
+	public String getSha1() {
+		return this.sha1;
+	}
+}
