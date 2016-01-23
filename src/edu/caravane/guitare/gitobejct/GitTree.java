04ea46d905df3b1cecea97cbe5f6fd320f01eda4
VieVie31 <orissermaroix@gmail.com>
2016-01-23 17:52:35 +0100

    Creation de la classe vide GitTree, elle hereite de GitObject

---
+++ src/edu/caravane/guitare/gitobejct/GitTree.java
@@ -0,0 +1,23 @@
+package edu.caravane.guitare.gitobejct;
+
+public class GitTree extends GitObject {
+
+	@Override
+	byte[] getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
