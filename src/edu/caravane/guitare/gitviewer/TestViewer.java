57d2d7608f51758953addc36deba7588d7da4287
VieVie31 <orissermaroix@gmail.com>
2016-01-25 09:19:55 +0100

    package du Viewer d'objects

---
+++ src/edu/caravane/guitare/gitviewer/TestViewer.java
@@ -0,0 +1,9 @@
+package edu.caravane.guitare.gitviewer;
+
+public class TestViewer {
+	//pour tester le viewer...
+	//Romain, fais tes dans cette classe, dans une nouvelle fenetre
+	//et une fois tout fini on importera ce package dans l'apprlication
+	//principale...
+	//Toutes les classes que tu fais doivent etre crees dans ce package...
+}
