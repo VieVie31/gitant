5b3fa9a4587c5327b1541884dc68c9934e2b482e
Romain <roch.romain@live.fr>
2016-01-26 09:45:03 +0100

    Rien de trop nouveau, des tests à intervalles espacés et irréguliers (par ci par là quoi). (putain les guillemets c'est chiant omgwtfbbq)

--- src/edu/caravane/guitare/gitviewer/TestViewer.java
+++ src/edu/caravane/guitare/gitviewer/TestViewer.java
@@ -1,12 +1,16 @@
 package application;
 	
+import java.util.ArrayList;
+
 import javafx.application.Application;
+import javafx.geometry.Bounds;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 
 
 public class Main extends Application {
@@ -14,12 +18,17 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Group root = new Group();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,500,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Viewer !");
 			primaryStage.setScene(scene);
-			ImageView imageAffiche = new ImageView(new Image(Main.class.getResourceAsStream("images/Aile de Mort VS Electromage.jpg")));
+			primaryStage.sizeToScene();
+			primaryStage.setMaximized(true);
+			ImageView imageAffiche = new ImageView(new Image(Main.class.getResourceAsStream("images/Aile de Mort VS Electromage.jpg"))); // Il faudra peut-être se servir de OutputStream pour obtenir l'image.
 			root.getChildren().add(imageAffiche);
+			imageAffiche.setFitWidth(scene.getWidth());
+			imageAffiche.setPreserveRatio(true);
+			// primaryStage.addEventHandler(primaryStage.isMaximized(), imageAffiche.setFitWidth(scene.getWidth()));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
