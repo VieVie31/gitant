ee9770c36d11f0b9b88f0baa109e1146cd19fb0e
Sylvain <sylvain.maillot1@gmail.com>
2016-02-15 11:13:15 +0100

    Methodes getNames() et getParentFiles() de GitObject.java modifies...

--- src/edu/caravane/guitare/gitobejct/GitObject.java
+++ src/edu/caravane/guitare/gitobejct/GitObject.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public abstract class GitObject implements GitObjectInterface {
 	protected String type, sha1;
 	protected int size;
-	
+
 	protected ArrayList<String> names = new ArrayList<String>();
 	protected ArrayList<String> parents = new ArrayList<String>();
 	protected GitDate date = new GitDate(0); //What I haven't for 14/02
@@ -13,16 +13,18 @@ public abstract class GitObject implements GitObjectInterface {
 	public abstract String getType();
 	public abstract int getSize();
 	public abstract String getId();
-	
+
 	public GitDate date() {
 		return date;
 	}
-	
+
 	@Override
 	public void setDate(GitDate date) {
+		System.out.println("J'aime bien les chips wesh");
 		this.date = date;
+		System.out.println(this.date);
 	}
-	
+
 	@Override
 	public void addName(String name) {
 		names.add(name);
@@ -32,7 +34,7 @@ public abstract class GitObject implements GitObjectInterface {
 	public void addParent(String parentFileName) {
 		parents.add(parentFileName);
 	}
-	
+
 	@Override
 	public void addParents(String[] parentsFilesNames) {
 		for (String parent : parentsFilesNames)
@@ -47,11 +49,11 @@ public abstract class GitObject implements GitObjectInterface {
 
 	@Override
 	public String[] getNames() {
-		return (String[]) names.toArray();
+		return names.toArray(new String[names.size()]);
 	}
 
 	@Override
 	public String[] getParentFiles() {
-		return (String[]) parents.toArray();
+		return parents.toArray(new String[parents.size()]);
 	}
 }
