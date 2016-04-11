314cdff18107f15a4f3ef1f11b7d17ae75e21312
Sylvain <sylvain.maillot1@gmail.com>
2016-04-11 20:31:34 +0200

    Refactor -> Supression de GitPack + ajout de la fonction pour les packs dans MainWindow

--- src/edu/caravane/guitare/application/MainWindow.java
+++ src/edu/caravane/guitare/application/MainWindow.java
@@ -19,15 +19,26 @@ import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.zip.DataFormatException;
+
+import org.eclipse.jgit.internal.storage.file.PackFile;
+import org.eclipse.jgit.internal.storage.file.PackIndex.MutableEntry;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryBuilder;
+
 import edu.caravane.guitare.gitobject.GitBlob;
 import edu.caravane.guitare.gitobject.GitCommit;
 import edu.caravane.guitare.gitobject.GitObject;
 import edu.caravane.guitare.gitobject.GitObjectReader;
 import edu.caravane.guitare.gitobject.GitObjectType;
 import edu.caravane.guitare.gitobject.GitObjectsIndex;
-import edu.caravane.guitare.gitobject.GitPack;
 import edu.caravane.guitare.gitobject.GitTag;
 import edu.caravane.guitare.gitobject.GitTree;
 import edu.caravane.guitare.gitobject.TreeEntry;
@@ -78,7 +89,7 @@ public class MainWindow extends Application {
 		for (String pathObj : listObjs) {
 			if (pathObj.contains(osBarre+"pack"+osBarre))
 				if (pathObj.contains(osBarre+"pack"+osBarre)){
-					GitPack.makePack(pathObj);
+					makePack(pathObj);
 					continue;
 				}
 			gor = new GitObjectReader(pathObj);
@@ -86,6 +97,57 @@ public class MainWindow extends Application {
 		}
 		return goi;
 	}
+
+	/**
+	 * Function used to treat pack object,
+	 * it load the data from each object of the pack to create the original object
+	 * when the object is recreate it store the object into the git object index
+	 *
+	 * @param pathPack
+	 * @throws IOException
+	 * @throws DataFormatException
+	 */
+		public static void makePack(String pathPack) throws IOException,
+		DataFormatException {
+			GitObjectsIndex goi =  GitObjectsIndex.getInstance();
+			String[] path = pathPack.split(osBarre+".git"+osBarre);
+			RepositoryBuilder builder = new RepositoryBuilder();
+			builder.setMustExist(true);
+			builder.setGitDir(new File(path[0]+ osBarre+".git"+osBarre));
+			Repository repository = builder.build();
+
+			File pa = new File(pathPack);
+			PackFile p = new PackFile(pa, 0);
+
+			for (MutableEntry mutableEntry : p) {
+				GitObject obj = null;
+				String sha1 = mutableEntry.toObjectId().name();
+				ObjectId id = repository.resolve(sha1);
+				ObjectLoader loader = repository.open(id);
+
+				switch (loader.getType()) {
+				case Constants.OBJ_BLOB:
+					obj = new GitBlob(loader.getSize(), sha1, "",
+							loader.getCachedBytes());
+					break;
+				case Constants.OBJ_COMMIT:
+					obj = new GitCommit(loader.getSize(), sha1,
+							new String(loader.getCachedBytes()));
+					break;
+				case Constants.OBJ_TREE:
+					obj = new GitTree(loader.getSize(), sha1,
+							loader.getBytes());
+					break;
+				case Constants.OBJ_TAG:
+					obj = new GitTag(loader.getSize(), sha1,
+							new String(loader.getCachedBytes()));
+				default:
+					break;
+				}
+
+					goi.put(mutableEntry.toObjectId().name(),obj);
+			}
+		}
 
 	/**
 	 * This function browse in a tree the find all his son and add son's name
